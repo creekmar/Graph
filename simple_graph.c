d13b79771f38789f09ef974a5496dec48b665ed0
Ming E Creekmore <mec5765@cs.rit.edu>
2022-03-03 19:19:58 -0500

    Implemented grh_load_file function and created a local function neighbor_exists for determining whether to add a new neighbor or not

--- simple_graph.c
+++ simple_graph.c
@@ -103,6 +103,76 @@ void grh_print_graph(ObjectList *graph) {
     free(node);
 }
 
+/// neighbor_exists determines whether a neighbor with a given name is in the list
+/// @param neighbors is a pointer to the list of neighbors
+/// @param name is the name of the neighbor
+static void neighbor_exists(ObjectList *neighbors, char *name) {
+    Iter *iterator = ol_iterator;
+    char tmp[MAX_NAME] = "";
+    while(ol_has_next(iterator)) {
+        tmp = (char *) ol_next(iterator);
+        if(strcmp(name, tmp) == 0) {
+            free(iterator);
+            return 1;
+        }
+    }
+    free(iterator);
+    return 0;
+}
+
+///loads a input from a stream and puts it into the graph
 void grh_load_file(ObjectList *graph, FILE *input) {
-    
+    char line[MAX_FILE_LINE_LENGTH];
+    fgets(line, MAX_FILE_LINE_LENGTH, input);
+    //There is actual input
+    if(strlen(line > 1)) {
+        char strip_line[MAX_FILE_LINE_LENGTH] = "";
+        strncpy(strip_line, line, strlen(line)-1);
+        GraphNode *node = (GraphNode *) malloc(sizeof(GraphNode));
+        char comma[2] = ",";
+        char *neighbors = strchr(strip_line, comma);
+        //if it has neighbors
+        if(neighbors != NULL) {
+            char *name[MAX_NAME] = "";
+            strncpy(name, line, (size_t)(neighbors-strip_line));
+            node = grh_find_node_by_name(name);
+            //if node not exist create one
+            if(node == NULL) {
+                node = grh_create_node(name);
+                ol_insert(graph, node);
+            }
+            neighbors ++; //make index point to after the comma
+            char *token = strtok(neighbors, comma); //separating neighbors
+            //add neighbors until empty
+            while(token != NULL) {
+                GraphNode *tmp = (GraphNode *) malloc(sizeof(GraphNode));
+                tmp = grh_find_node_by_name(token);
+                //if neighbor not in graph add new node
+                if(tmp == NULL) {
+                    tmp = grh_create_node(token);
+                    ol_insert(tmp->neighbors, token);
+                    ol_insert(graph, tmp);
+                }
+                //neighbor is in graph, insert the first name as a neighbor if need
+                else if(!neighbor_exists(tmp->neighbors, name)) {
+                    ol_insert(tmp->neighbors, name);
+                }
+                //insert neighbor for first name if not there
+                if(!neighbor_exists(node->neighbors, token)) {
+                    ol_insert(node->neighbors, token);
+                }
+                free(tmp);
+                token = strtok(NULL, comma);
+            }    
+        }
+        //if no neighbors
+        else {
+            node = grh_find_node_by_name(strip_line);
+            if(node == NULL) {
+                node = grh_create_node(name);
+                ol_insert(graph, node);
+            }
+        }
+        free(node);
+    } //there is no input so skip blank line
 }
